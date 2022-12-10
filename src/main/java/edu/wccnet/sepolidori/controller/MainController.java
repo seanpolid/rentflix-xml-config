b9006bda6c1335df02894f220f2412314cb4298f
Sean Polidori <seanpolidori0@gmail.com>
2022-12-10 16:18:49 -0500

    Implemented mapping for home and login

--- src/main/java/edu/wccnet/sepolidori/controller/MainController.java
+++ src/main/java/edu/wccnet/sepolidori/controller/MainController.java
@@ -1,10 +1,19 @@
 package edu.wccnet.sepolidori.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class MainController {
 	
+	@RequestMapping({"/home", "/"})
+	public String home() {
+		return "index";
+	}
+	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}
 }
