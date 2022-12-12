f848f1e6736879c1838eb919e9e68a39ae426df2
Sean Polidori <seanpolidori0@gmail.com>
2022-12-11 20:12:04 -0500

    Updated fetchtype

--- src/main/java/edu/wccnet/sepolidori/entity/Movie.java
+++ src/main/java/edu/wccnet/sepolidori/entity/Movie.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,6 +18,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Fetch;
 import org.springframework.stereotype.Component;
 
 @Entity
@@ -56,7 +58,7 @@ public class Movie {
 	@JoinColumn(name="genre_id")
 	private Genre genre;
 	
-	@OneToMany(mappedBy="movie")
+	@OneToMany(mappedBy="movie", fetch=FetchType.LAZY)
 	private List<InvoiceMovie> invoiceMovies = new ArrayList<InvoiceMovie>();
 
 	public int getId() {
