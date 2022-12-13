68d34795778497ea5306c21b7b79be9da19e2f4c
Sean Polidori <seanpolidori0@gmail.com>
2022-12-12 19:21:28 -0500

    Created mapping to commit checkout to database

--- src/main/java/edu/wccnet/sepolidori/controller/MainController.java
+++ src/main/java/edu/wccnet/sepolidori/controller/MainController.java
@@ -1,5 +1,8 @@
 package edu.wccnet.sepolidori.controller;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -7,14 +10,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.wccnet.sepolidori.entity.Customer;
+import edu.wccnet.sepolidori.entity.Invoice;
+import edu.wccnet.sepolidori.entity.InvoiceMovie;
 import edu.wccnet.sepolidori.entity.Movie;
 import edu.wccnet.sepolidori.service.CartService;
 import edu.wccnet.sepolidori.service.CustomerService;
 import edu.wccnet.sepolidori.service.GenreService;
+import edu.wccnet.sepolidori.service.InvoiceService;
 import edu.wccnet.sepolidori.service.LoggedInUserService;
 import edu.wccnet.sepolidori.service.LoginProcessor;
 import edu.wccnet.sepolidori.service.MovieService;
@@ -30,13 +37,16 @@ public class MainController {
 	private final RatingService ratingService;
 	private final MovieService movieService;
 	private final CartService cartService;
+	private final InvoiceService invoiceService;
 	
 	public MainController(CustomerService customerService, 
 						LoggedInUserService loggedInUserService, 
 						LoginProcessor loginProcessor, 
 						RatingService ratingService, 
 						GenreService genreService, 
-						MovieService movieService, CartService cartService) {
+						MovieService movieService, 
+						CartService cartService, 
+						InvoiceService invoiceService) {
 		this.loginProcessor = loginProcessor;
 		this.customerService = customerService;
 		this.loggedInUserService = loggedInUserService;
@@ -44,6 +54,7 @@ public class MainController {
 		this.ratingService = ratingService;
 		this.movieService = movieService;
 		this.cartService = cartService;
+		this.invoiceService = invoiceService;
 	}
 	
 	@RequestMapping({"/home", "/"})
@@ -90,16 +101,46 @@ public class MainController {
 		return "redirect:/browse";
 	}
 	
-	@RequestMapping("/checkout")
+	@GetMapping("/checkout")
 	public String checkout(Model model) {
 		model.addAttribute("customer", loggedInUserService.getCustomer());
 		model.addAttribute("movies", cartService.getMovies());
 		return "checkout";
 	}
 	
+	@PostMapping("/checkout")
+	public String checkout() {
+		Invoice invoice = new Invoice();
+		BigDecimal total = new BigDecimal(0);
+		for (Movie movie : cartService.getMovies()) {
+			InvoiceMovie invoiceMovie = new InvoiceMovie();
+			invoiceMovie.setId(0);
+			invoiceMovie.setMovie(movie);
+			invoiceMovie.setInvoice(invoice);
+			invoice.addInvoiceMovie(invoiceMovie);
+			System.out.println(movie.getCost());
+			total.add(movie.getCost());
+			System.out.println(total);
+		}
+		invoice.setCustomer(loggedInUserService.getCustomer());
+		invoice.setTotal(total);
+		invoice.setDate(LocalDateTime.now());
+		invoiceService.saveInvoice(invoice);
+		return "redirect:/library";
+	}
+	
+	@RequestMapping("/library")
+	public String library(Model model) {
+		Customer customer = loggedInUserService.getCustomer();
+		model.addAttribute("customer", customer);
+		model.addAttribute("movies", invoiceService.getInvoices(customer.getId()));
+		return "library";
+	}
+	
 	@RequestMapping("/logout")
 	public String logout() {
 		loggedInUserService.setCustomer(null);
+		cartService.setMovies(null);
 		return "redirect:/";
 	}
 	
