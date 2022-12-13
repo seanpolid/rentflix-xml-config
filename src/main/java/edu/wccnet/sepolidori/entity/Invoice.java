e988eca9893de1013ec7c6f2aa8cdef1200a8b43
Sean Polidori <seanpolidori0@gmail.com>
2022-12-12 21:51:32 -0500

    Specified fetch type for invoiceMovies attribute

--- src/main/java/edu/wccnet/sepolidori/entity/Invoice.java
+++ src/main/java/edu/wccnet/sepolidori/entity/Invoice.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -34,7 +35,7 @@ public class Invoice {
 	@JoinColumn(name="customer_id")
 	private Customer customer;
 	
-	@OneToMany(mappedBy="invoice", cascade=CascadeType.ALL)
+	@OneToMany(mappedBy="invoice", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
 	private List<InvoiceMovie> invoiceMovies = new ArrayList<InvoiceMovie>();
 
 	public int getId() {
